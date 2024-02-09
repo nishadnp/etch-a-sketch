cb7cfdf562e123f2e1ea3d6564626a36174c28ca
Nishad <69951135+nishadnp@users.noreply.github.com>
2024-02-10 01:04:11 +0530

    feat: Set up a hover effect to grid-square divs
    Color changes to black when mouse cursor passes over grid-square divs.
    Leaves a pixelated trail through the grid like a pen would.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container');
 
-const GRID_ORDER= 16;
+const GRID_ORDER = 16;
 // Create 256 divs inside #container div
 for (let i = 1; i <= GRID_ORDER ** 2; i++) {
     const gridSquare = document.createElement('div');
@@ -8,3 +8,14 @@ for (let i = 1; i <= GRID_ORDER ** 2; i++) {
     gridSquare.id = `grid-square-${i}`;
     container.appendChild(gridSquare);
 }
+
+
+const gridSquares = document.querySelectorAll('.grid-square');
+
+// For each square in grid
+gridSquares.forEach((square) => {
+    // Fill black color in square div when mouse cursor passes over it.
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = "black";
+    })
+});
