c888e3d9c8807e4ed0683b491019b8d67761b0cf
Nishad <69951135+nishadnp@users.noreply.github.com>
2024-02-11 22:55:11 +0530

    feat: Add a button to reset the grid
    Existing grid will be removed.
    New grid with new order given by user through prompt is created.
    Variable newGridOrder should be a numeric value between 16 to 100.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,56 @@
-const container = document.getElementById('container');
+function createGrid(gridOrder) {
+    // Create 256 divs inside #container div
+    for (let i = 1; i <= gridOrder ** 2; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridSquare.id = `grid-square-${i}`;
+        container.appendChild(gridSquare);
+    }
+}
 
-const GRID_ORDER = 16;
-// Create 256 divs inside #container div
-for (let i = 1; i <= GRID_ORDER ** 2; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    gridSquare.id = `grid-square-${i}`;
-    container.appendChild(gridSquare);
+function removeGrid(gridOrder) {
+    for (let i = 1; i <= gridOrder ** 2; i++) {
+        const gridSquare = document.querySelector('.grid-square');
+        container.removeChild(gridSquare);
+    }
 }
 
 
-const gridSquares = document.querySelectorAll('.grid-square');
+// <-------Main code------->
+
+const container = document.getElementById('container');
+// Default gridOrder value
+let gridOrder = 16;
+// Initial grid
+createGrid(gridOrder);
 
 // For each square in grid
-gridSquares.forEach((square) => {
+document.querySelectorAll('.grid-square').forEach((square) => {
     // Fill black color in square div when mouse cursor passes over it.
     square.addEventListener('mouseover', () => {
         square.style.backgroundColor = "black";
-    })
+    });
+});
+
+
+const resetButton = document.getElementById('grid-reset');
+
+resetButton.addEventListener('click', () => {
+    // Prompt user new grid size
+    let newGridOrder = parseInt(prompt("Enter New Grid Size (16 - 100): "));
+
+    // If true, reset grid
+    if (!isNaN(newGridOrder) && (newGridOrder >= 16 && newGridOrder <= 100)) 
+    {
+        // Remove existing grid
+        removeGrid(gridOrder);
+
+        gridOrder = newGridOrder;
+        // New grid
+        createGrid(gridOrder);
+    }
+    else 
+    {
+        alert("Invalid Input!");
+    }
 });
